1790c389ed0286fbb5ffebc8f48f8f600026bffc
kris-randen <kris.randen@gmail.com>
2021-09-15 13:24:37 +0530

    Project created.

---
+++ udemy-clone-twitter/udemy-clone-twitter/udemy_clone_twitterApp.swift
@@ -0,0 +1,17 @@
+//
+//  udemy_clone_twitterApp.swift
+//  udemy-clone-twitter
+//
+//  Created by Krishnaswami Rajendren on 9/15/21.
+//
+
+import SwiftUI
+
+@main
+struct udemy_clone_twitterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
